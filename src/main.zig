289d1c03cba49828163164d64bef1a8d3b1ec426
Write Int <151211283+WriteNaN@users.noreply.github.com>
2024-05-10 17:50:34 +0000

    Temp remove dynamic serve

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,4 @@
 const std = @import("std");
-const Server = @import("server.zig");
+const serve = @import("serve.zig");
 
-pub fn main() !void {
-    var allocator = std.heap.raw_c_allocator;
-
-    const app = Server.init(&allocator);
-    defer app.deinit();
-
-    const port: u16 = 3000;
-    const folder = std.fs.cwd().openDir("public", .{.iterate = true}) catch unreachable;
-
-    //app.static(.{ .folder = folder, .route = "" });
-    app.dynamic(.{ .folder = folder, .route = "public" });
-
-    try app.listen(.{
-        .port = port
-    });
-}
+const Self = @This();
--- src/serve.zig
+++ src/serve.zig
@@ -16,7 +16,7 @@ var dynamic_map: std.StringHashMap(std.fs.Dir) = undefined;
 /// Initialize the server.
 pub fn init(allocator: *std.mem.Allocator) Server {
     map = std.StringHashMap([]const u8).init(allocator.*);
-    dynamic_map = std.StringHashMap(std.fs.Dir).init(allocator.*);
+    //dynamic_map = std.StringHashMap(std.fs.Dir).init(allocator.*);
     return Server{ .allocator = allocator };
 }
 
@@ -64,19 +64,6 @@ pub fn static(self: Server, opts: ServeOptions) void {
     }
 }
 
-/// #### Serve files in a directory.
-/// ```zig
-/// <app>.dynamic(.{ .folder = folder, .root = "assets" });
-/// ```
-/// it is only recommended to use this function if new files are created/existing deleted
-/// on the directory that is being served. otherwise `<app>.static` is probably what you're looking for.
-pub fn dynamic(self: Server, opts: ServeOptions) void {
-    _ = self;
-    // for use on quick checks.
-    if (!isDynamicDirRegistered) isDynamicDirRegistered = true;
-    dynamic_map.put(opts.route, opts.folder) catch unreachable;
-}
-
 const ListenOpts = struct { port: u16, host: ?[]const u8 = null };
 
 /// Starts the server on the specified port.
@@ -110,45 +97,8 @@ pub fn listen(self: Server, opts: ListenOpts) !void {
                 }
                 try req.respond(content, .{ .status = .ok, .transfer_encoding = .chunked });
             } else {
-                // mock
-                const uri = try std.fmt.allocPrint(self.allocator.*, "https://localhost:2000{s}", .{req.head.target});
-                defer self.allocator.*.free(uri);
-                const parsed = try std.Uri.parse(uri);
-
-                var key_iterator = std.mem.splitSequence(u8, parsed.path.percent_encoded, "/");
-                const key = key_iterator.next();
-                const routeKey = key_iterator.next();
-                if (isDynamicDirRegistered and (key != null or routeKey != null) and (dynamic_map.contains(key.?) or dynamic_map.contains(routeKey.?))) {
-                    const dir = dynamic_map.get(key.?) orelse dynamic_map.get(routeKey.?);
-                    const shouldTrim = dynamic_map.get(key.?) == null and true;
-                    std.debug.print("{any}\n", .{shouldTrim});
-                    if (dir != null) {
-                        const realPath = dir.?.realpathAlloc(self.allocator.*, ".") catch unreachable;
-
-                        const path = parsed.path.percent_encoded[1..(parsed.path.percent_encoded.len - 1)];
-
-                        const absolutePath = try std.fmt.allocPrint(self.allocator.*, "{s}/{s}", .{ realPath, path });
-
-                        std.debug.print("{s}\n", .{absolutePath});
-                        const file = std.fs.openFileAbsolute(absolutePath, .{ .mode = .read_only }) catch |err| {
-                            switch (err) {
-                                error.FileNotFound => {
-                                    try handle(.not_found, req);
-                                    continue :serve;
-                                },
-                                else => {
-                                    try handle(.internal_server_error, req);
-                                    continue :serve;
-                                },
-                            }
-                        };
-
-                        const content = try file.readToEndAlloc(self.allocator.*, (try file.stat()).size);
-                        try req.respond(content, .{ .transfer_encoding = .chunked, .status = .ok });
-                    }
-                } else try handle(.not_found, req);
+                handle(.not_found, req);
             }
-            continue :serve;
         }
     }
 }
@@ -159,6 +109,6 @@ fn handle(status: Http.Status, req: *Http.Server.Request) !void {
 
 pub fn deinit(self: Server) void {
     _ = self;
-    dynamic_map.deinit();
+    //dynamic_map.deinit();
     map.deinit();
 }
